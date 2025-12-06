e0d322e493e9349a975f858ab49fa3c481f5970d
Omaima Sabri <omaimasabri81@gmail.com>
2025-12-06 19:13:31 +0100

    Fusion PR feat/ajout-models-cabinet + mes modifications frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import PatientsList from "./components/patient/ PatientList.jsx";
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <PatientsList />
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { I18nProvider } from './i18n.jsx';
+import { ThemeProvider } from './contexts/ThemeContext';
+import './pages/global.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <ThemeProvider>
+            <I18nProvider>
+                <App />
+            </I18nProvider>
+        </ThemeProvider>
+    </React.StrictMode>
+);
